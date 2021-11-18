8c8d358236b99a5ae4ce07e85df27142bbff4587
Daniel J <48568101+daniel-jaimes@users.noreply.github.com>
2021-11-19 00:18:14 +0100

    refactor Screen Reader

--- src/Dao/ScreenReader.java
+++ src/Dao/ScreenReader.java
@@ -1,45 +1,22 @@
 package Dao;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class ScreenReader {
-    private static ArrayList<char[][]> screens;
-    public static ArrayList<char[][]> getInstance(){
-        if(screens == null){
-            BufferedReader BR;
+    private static BufferedReader BR = null;
+    private ScreenReader(){
+
+    }
+    public static BufferedReader getInstance(){
+        if(BR == null){
             try {
                 BR = new BufferedReader(new FileReader("files/pantallas.txt"));
-                String str;
-                boolean endreader = false;
-                int index = 0, row = 0;
-                while((str = BR.readLine()) != null && !endreader){
-                    char firstChar = str.charAt(0);
-                    char[][] screen = new char[5][10];
-                    if(str.length() == 2 && firstChar == '#'){
-                        if(row > 0) screens.add(screen);
-                        row = 0;
-                    }
-                    if(str.length() == 1 && firstChar == '#'){
-                        endreader = true;
-                    }
-                    if(str.length() > 2){
-                        String[] arrayStr = str.split(" ");
-                        str = Arrays.toString(arrayStr);
-                        screen[row] = str.toCharArray();
-
-                        row++;
-                    }
-
-                }
-            } catch (IOException e) {
+            } catch (FileNotFoundException e) {
                 System.out.println(e.getMessage());
             }
-
         }
-        return screens;
+        return BR;
     }
 }
