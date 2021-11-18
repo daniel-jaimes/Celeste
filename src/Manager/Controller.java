ee662e6ac58d5e3750a75687dc1cd2d24df4bb43
Daniel J <48568101+daniel-jaimes@users.noreply.github.com>
2021-11-19 00:18:36 +0100

    Refactor Controller for Screen Reader

--- src/Manager/Controller.java
+++ src/Manager/Controller.java
@@ -5,49 +5,73 @@ import Dao.InputReader;
 import Dao.ScreenReader;
 import Model.Player;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Controller {
-    private static Controller controller;
+    //SINGLETON
+    private static final Controller controller;
+    static {
+        controller = new Controller();
+    }
     private Controller(){
     }
-    //SINGLETON
     public static Controller getInstance(){
-        if(controller == null){
-            controller = new Controller();
-        }
         return controller;
     }
     
     public void init() {
         ArrayList<Player> players;
-        ArrayList<char[][]> levels;
+        ArrayList<Character[][]> levels;
         levels = readScreens();
         //TODO generating salida.xml with the information merge it of their levels and players for each they
         players = readPlayers();
-        printPlayers(players);
-        printLevels(levels);
+        writeOutput(levels, players);
     }
 
-    private void printLevels(ArrayList<char[][]> levels) {
-        levels.forEach(screen -> {
-            for (char[] row : screen) {
-                for (char element : row){
-                    System.out.println(element + " ");
-                }
-            }
-        });
-    }
+    private void writeOutput(ArrayList<Character[][]> levels, ArrayList<Player> players) {
 
-    private void printPlayers(ArrayList<Player> players) {
-        players.forEach(p -> System.out.println(p.toString()));
     }
 
     private ArrayList<Player> readPlayers() {
         return InputReader.getInstance().getPlayers();
     }
 
-    private ArrayList<char[][]> readScreens() {
-        return ScreenReader.getInstance();
+    private ArrayList<Character[][]> readScreens() {
+        ArrayList<Character[][]> screens;
+        screens = new ArrayList<>();
+        String str;
+        boolean endReader = false;
+        ArrayList<Character[]> display = new ArrayList<>();
+        try {
+            BufferedReader BR = ScreenReader.getInstance();
+            while((str = BR.readLine()) != null && !endReader){
+                comands(str, endReader, display, screens);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return screens;
+    }
+    private void comands(String str, boolean endReader, ArrayList<Character[]> screen,
+                         ArrayList<Character[][]> screens) {
+        char firstChar = str.charAt(0);
+        if(firstChar == '#' && screen.size() != 0) {
+            Character[][] display = new Character[screen.size()][screen.get(0).length];
+            screen.toArray(display);
+            screens.add(display);
+        }
+        if(str.length() == 1 && firstChar == '#'){
+            endReader = true;
+        }
+        if(str.length() == 2 && firstChar == '#'){
+            screen.clear();
+        }
+        if(str.length() > 2){
+            str = str.replaceAll("\\s+", "");
+            //recorro el string pasandolo a un array de Character y luego lo añado.
+            screen.add(str.chars().mapToObj(c -> (char)c).toArray(Character[]::new));
+        }
     }
 }
