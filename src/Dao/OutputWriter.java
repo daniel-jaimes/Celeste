b94c65a5979473b18f08a4745d0e6992c9e6914d
Daniel Jaimes <48568101+daniel-jaimes@users.noreply.github.com>
2021-11-19 19:47:09 +0100

    create and finish

--- src/Dao/OutputWriter.java
+++ src/Dao/OutputWriter.java
@@ -1,5 +1,43 @@
 package Dao;
 
-public class OutputWriter {
+import org.w3c.dom.Document;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class OutputWriter {
+    private Document document;
+
+    public OutputWriter(){
+        DocumentBuilderFactory factoria = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder builder = factoria.newDocumentBuilder();
+            document = builder.newDocument();
+        } catch (ParserConfigurationException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public Document getDocument() {
+        return document;
+    }
+
+    public void generateXml(){
+        TransformerFactory factory = TransformerFactory.newInstance();
+        try {
+            Transformer transformer = factory.newTransformer();
+            Source source = new DOMSource(document);
+            PrintWriter pw = new PrintWriter("files/salida.xml");
+            Result result = new StreamResult(pw);
+            transformer.transform(source, result);
+        } catch (FileNotFoundException | TransformerException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
